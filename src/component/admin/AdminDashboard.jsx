df24cdee0c0a8c6e03b79b7c9fe6d4352e316409
Kavindu-Madhushanka <madu10275@gmail.com>
2026-01-25 09:20:44 +0530

    add statcard to admindashborad

--- src/component/admin/AdminDashboard.jsx
+++ src/component/admin/AdminDashboard.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Sidebar from "./sidebar";
+import StatCard from "./StatCard";
+import { FaRegUser } from "react-icons/fa";
+import { FaUserCheck } from "react-icons/fa6";
+import { SlCalender } from "react-icons/sl";
+import { IoIosAlert } from "react-icons/io";
 const AdminDashboard = () => {
   return (
     <div className="flex bg-gradient-to-br from-[#0f172a] via-[#1e1b4b] to-[#4c1d95] min-h-screen text-white font-sans">
@@ -17,6 +22,33 @@ const AdminDashboard = () => {
             {/* profile and notification ui here */}
           </div>
         </header>
+        {/* stats grid */}
+        <div className="grid grid-cols-4 gap-6 mb-10">
+          <StatCard
+            label="Total Students"
+            value="4,281"
+            icon={FaRegUser}
+            colorClass="bg-blue-600"
+          />
+          <StatCard
+            label="Active Faculty"
+            value="156"
+            icon={FaUserCheck}
+            colorClass="bg-purple-600"
+          />
+          <StatCard
+            label="Avg Attendance"
+            value="92.4%"
+            icon={SlCalender}
+            colorClass="bg-green-600"
+          />
+          <StatCard
+            label="Open Tickets"
+            value="14"
+            icon={IoIosAlert}
+            colorClass="bg-orange-600"
+          />
+        </div>
       </main>
     </div>
   );
