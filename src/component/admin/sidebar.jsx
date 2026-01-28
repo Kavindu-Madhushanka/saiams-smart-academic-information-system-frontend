23af4758a85a2130c23f993c6423d68f5674e9f4
Kavindu-Madhushanka <madu10275@gmail.com>
2026-01-28 11:03:46 +0530

    modify

--- src/component/admin/sidebar.jsx
+++ src/component/admin/sidebar.jsx
@@ -9,6 +9,7 @@ import { IoIosAlert } from "react-icons/io";
 import { IoIosSettings } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
 import { Link } from "react-router-dom";
+import logoimage from "../../assets/universitylogo.jpg";
 const SidebarItem = ({ icon: Icon, label, active }) => (
   <div className="flex items-center gap-3 p-3 text-white transition-all rounded-lg cursor-pointer hover:bg-blue-600">
     <Icon size={20} />
@@ -19,8 +20,8 @@ const Sidebar = () => {
   return (
     <div className="w-64 h-screen bg-[#0f111a] p-4 flex flex-col border-r border-gray-800 fixed">
       <div className="flex items-center gap-2 px-2 mb-8 text-xl font-bold text-blue-400">
-        <div className="flex items-center justify-center w-8 h-8 italic text-white bg-blue-500 rounded">
-          S
+        <div className="flex items-center justify-center italic text-white rounded-full">
+          <img src={logoimage} className="w-12 h-12"></img>
         </div>
         SAIAMS
       </div>
@@ -33,7 +34,9 @@ const Sidebar = () => {
         <Link to={"/lecturemanagement"}>
           <SidebarItem icon={FaUserCheck} label="Lecturer Management" />
         </Link>
-        <SidebarItem icon={FaBookOpen} label="Assign Subjects" />
+        <Link to={"/subjectmanagement"}>
+          <SidebarItem icon={FaBookOpen} label="Subjects Management" />
+        </Link>
         <SidebarItem icon={SlCalender} label="Attendance" />
         <SidebarItem icon={AiFillFileText} label="Results & ICA" />
         <SidebarItem icon={IoIosAlert} label="Issue Reports" />
